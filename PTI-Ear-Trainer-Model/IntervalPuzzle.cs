4a1b3195743cfd858057769efd21f5ee2edf0b2f
Lámfalusi Ádám <jnjwrg@inf.elte.hu>
2022-10-21 00:30:49 +0200

    Added new class representing a puzzle for the user and changed access modifiers in the model to help the new class

---
+++ PTI-Ear-Trainer-Model/IntervalPuzzle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTI_Ear_Trainer_Model
+{
+    internal class IntervalPuzzle
+    {
+        public Note Note1 { get; private set; }
+        public Note Note2 { get; private set; }
+
+        private Interval _interval;
+
+        public Interval GetInterval()
+        {
+            return this._interval;
+        }
+
+        public void SetInterval(Interval interval)
+        {
+            this._interval = interval;
+        }
+        public void SetInterval(Note note1, Note note2)
+        {
+            this._interval = PTI_Ear_Trainer_Model.CountInterval(note1, note2);
+        }
+    }
+}
--- PTI-Ear-Trainer-Model/PTI-Ear-Trainer-Model.cs
+++ PTI-Ear-Trainer-Model/PTI-Ear-Trainer-Model.cs
@@ -2,14 +2,26 @@
 {
     public class PTI_Ear_Trainer_Model
     {
-        Interval CountInterval(Note note1, Note note2)
+        private Random rng;
+
+        public PTI_Ear_Trainer_Model()
+        {
+            this.rng = new Random();
+        }
+
+        static internal Interval CountInterval(Note note1, Note note2)
         {
             return (Interval)Math.Abs((int)note2 - (int)note1);
         }
 
-        Note CountNote (Note note, Interval interval)
+        static internal Note CountNote (Note note, Interval interval)
         {
             return (Note)(((int)note + (int)interval) % 12);
+        }
+
+        private IntervalPuzzle GenerateInterval()
+        {
+            throw new NotImplementedException();
         }
     }
 }
