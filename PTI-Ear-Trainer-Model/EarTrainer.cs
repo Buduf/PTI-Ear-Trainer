d139fec7d671170379b2df5e5e1941db62081da7
Andre Bilik <w8i2pe@inf.elte.hu>
2022-11-18 12:37:36 +0100

    Set project meta information, Code cleanup, Disabled Implicit usings, Removed unnecessary usings

--- PTI-Ear-Trainer-Model/EarTrainer.cs
+++ PTI-Ear-Trainer-Model/EarTrainer.cs
@@ -40,11 +40,8 @@ namespace PTI_Ear_Trainer_Model
 
         public static Interval CountInterval(Note note1, Note note2)
         {
-            //return (Interval)(((int)note1 >= (int)note2) ? ((int)note1 - (int)note2) : ((int)note2 - (int)note1));
             if (note2 >= note1) return (Interval)((int)note2 - (int)note1);
             throw new ArgumentException("First note must be lower than second note!");
-            //return (Interval)((int)note2 + Enum.GetNames(typeof(Note)).Length - (int)note1);
-
         }
 
         public static Note CountNote(Note note, Interval interval)
